19d90b37170facfd06c2286a5f88e8ca23bdb311
jsabarre <101218249+jsabarre@users.noreply.github.com>
2022-03-18 20:22:25 -0400

    Add files via upload

---
+++ Assignment7/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        public static void Main()
+        {
+
+            //int[] numbers = GenerateNumbers(20);
+            //Reverse(numbers);
+            //Console.WriteLine("\n");
+            //PrintNumbers(numbers);
+
+            Fibonacci(7);
+        }
+        public static int[] GenerateNumbers(int n)
+        {
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i);
+
+                numbers[i] = i + 1;
+            }
+            return numbers;
+
+        }
+        public static void Reverse(int[] array)
+        {
+            for (int i = 0, j = array.Length - 1; i < j; i++, j--)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        public static void PrintNumbers(int[] numbers)
+        {
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+
+
+
+
+        //Question2
+
+        public static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            int a = 1, b = 1, c = 0;
+
+            for (int j = 3; j <= n; j++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            Console.WriteLine(c);
+
+            return c;
+
+
+        }
+    }
+}
